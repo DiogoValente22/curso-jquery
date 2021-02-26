5f9af26d9df0300945d5ccfb1382533eeb548af9
Diogo Valente de Aragão <diogovalentearagao@outlook.com>
2021-02-26 13:40:26 -0300

    add floor change

---
+++ Projeto3-elevador/Elevator.js
@@ -0,0 +1,64 @@
+class Elevator {
+
+    constructor() {
+
+        this.$elevator = $('.elevator');
+        this.floorQtd = 3;
+
+    }
+
+    openDoor() {
+
+        if(this.isDoorsOpen()){
+
+            return true;
+
+        }else {
+
+            this.$elevator.find('.door').addClass('open');
+
+        }
+
+        
+
+    }
+    closeDoor() {
+
+        if(this.isDoorsOpen()){
+
+            this.$elevator.find('.door').removeClass('open');
+
+        }else {
+            return true;
+        }
+
+    }
+
+    isDoorsOpen() {
+        let doors = this.$elevator.find('.door');
+
+        // if ternario, vai retornar true ou false
+        return (doors.hasClass('open'));
+         
+    }
+
+    goToFloor(number){
+
+        this.removeFloorClasses();
+
+        this.$elevator.addClass(`floor${number}`);
+
+        this.$elevator.data('floor', number);
+
+    }
+    removeFloorClasses() {
+
+        for(let i = 1; i <= this.floorQtd; i++) {
+
+            this.$elevator.removeClass(`floor${i}`);
+
+        }
+
+    }
+
+}
